df23b99fd7222225cd5856a94794ae5000b5ec3e
Amine Merad <meradamine2003@gmail.com>
2025-05-14 12:41:31 +0100

    correct ip add

--- nfs-server/start.sh
+++ nfs-server/start.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-# Start required services
+# Create required directories
+mkdir -p /run/sendsigs.omit.d
+mkdir -p /var/lib/nfs/rpc_pipefs
+mkdir -p /var/lib/nfs/v4recovery
+
+# Start required services in order
 service rpcbind start
+sleep 2
+
+# Configure exports before starting NFS
+exportfs -ra
+
+# Start NFS server
 service nfs-kernel-server start
+sleep 2
+
+# Show exports for debugging
+exportfs -v
+
+# Show RPC services
+rpcinfo -p
 
 # Keep container running
 tail -f /dev/null
